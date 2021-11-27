f780f455877aef8061057f34abfd2d0fb9f5744b
Charlie Klever <klever.charlie@gmail.com>
2021-11-27 11:02:04 -0800

    Create tileset.tsx

---
+++ tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="tileset" tilewidth="8" tileheight="8" tilecount="1024" columns="32">
+ <image source="E:/Dropbox/Galvatron/01Fundamentals/03School/01WSU/DTC392/tileset.png" width="256" height="256"/>
+</tileset>
