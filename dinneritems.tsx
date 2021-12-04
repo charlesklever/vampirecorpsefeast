1d3b2768d37c9a1eb29456319ecc1cc42e2bcd1c
Charlie Klever <klever.charlie@gmail.com>
2021-12-04 13:00:47 -0800

    Dining rooms

---
+++ dinneritems.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="dinneritems" tilewidth="8" tileheight="8" tilecount="360" columns="20">
+ <image source="../../../Desktop/GameDev/VampireCorpseFeast/Vampire Corpse Feast/Mansion-Interior-Assets-Pack/Murder Mansion Assets/dinneritems.png" width="160" height="144"/>
+</tileset>
